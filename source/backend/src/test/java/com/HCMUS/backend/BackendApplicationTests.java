25c23cec17545322838987a452814286c20dc6d7
Phon <ndphon23@clc.fitus.edu.vn>
2025-08-31 10:35:20 +0700

    reconstructor

---
+++ source/backend/src/test/java/com/HCMUS/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HCMUS.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
