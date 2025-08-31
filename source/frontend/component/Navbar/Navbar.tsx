c4d1f670e539c1dc3a26b56865e1b4015d19335b
Phon <ndphon23@clc.fitus.edu.vn>
2025-08-31 16:21:33 +0700

    update navbar having authentication button

--- source/frontend/component/Navbar/Navbar.tsx
+++ source/frontend/component/Navbar/Navbar.tsx
@@ -1,24 +1,25 @@
+"use client";
 import styles from "./Navbar.module.css";
 import Link from "next/link";
+
+import Auth from "../AuthenticationButton/Auth";
 
 export default function Navbar() {
     return (
         <header className={styles.navbar}>
             <div className={styles.logo}>
-                <Link href="/">
+                <Link href="/home">
                     <img src="/logo.png" alt="Logo" className={styles.logoImage} />
                 </Link>
             </div>
             <div className={styles.navLinks}>
                 <Link href="/about" className={styles.navLink}>About</Link>
                 <Link href="/projects" className={styles.navLink}>Projects</Link>
-                <Link href="/contact" className={styles.navLink}>Contact</Link>
                 <Link href="/uploading" className={styles.navLink}>Uploading</Link>
                 <Link href="/uploaded" className={styles.navLink}>Uploaded</Link>
             </div>
             <div className={styles.Authentication}> 
-                <button className={styles.loginButton}>Login</button>
-                <button className={styles.signupButton}>Sign Up</button>
+                <Auth />
             </div>
         </header>
     );
