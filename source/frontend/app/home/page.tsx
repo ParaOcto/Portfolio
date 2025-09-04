4043cb34f24056a7ca97faae6e9da509d1666b34
Phon <ndphon23@clc.fitus.edu.vn>
2025-09-04 15:09:56 +0700

    add source

--- source/frontend/app/home/page.tsx
+++ source/frontend/app/home/page.tsx
@@ -5,7 +5,6 @@ import { SiThreads } from "react-icons/si";
 
 
 export default async function Home(){
-    // Thêm delay 2 giây để test loading
     await new Promise(res => setTimeout(res, 1000));
     return (
         <div className={styles.container}>
