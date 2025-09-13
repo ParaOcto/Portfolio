74a4390a55dd8e211d9cbb56bd2416f4b41e1b26
Phon <ndphon23@clc.fitus.edu.vn>
2025-09-13 16:19:22 +0700

    reconstructor database

--- source/backend/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (username, password_hash, roleUser)
-VALUES ('NguyenDangPhon', 'personalproject', 'Phon')
-ON DUPLICATE KEY UPDATE username=username;
--- source/backend/src/main/resources/schema.sql
+++ source/backend/src/main/resources/schema.sql
@@ -2,23 +2,15 @@ CREATE TABLE IF NOT EXISTS users (
     userId INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password_hash VARCHAR(100) NOT NULL,
-    roleUser ENUM('Phon', 'Anonymous')
-
+    roleUser ENUM('Phon', 'user'),
+    avatar VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS post (
     postId INT AUTO_INCREMENT PRIMARY KEY,
     content TEXT NOT NULL,
-    userId INT,
-    FOREIGN KEY (userId) REFERENCES users(userId)
-);
-
-CREATE TABLE IF NOT EXISTS comment (
-    commentId INT AUTO_INCREMENT PRIMARY KEY,
-    content TEXT NOT NULL,
-    postId INT,
-    userId INT,
-    FOREIGN KEY (postId) REFERENCES post(postId),
-    FOREIGN KEY (userId) REFERENCES users(userId)
+    image VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
